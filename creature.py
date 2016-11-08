fb739b4aaec1aea4b9836eeeeba06e9d7d7b86dd
Xiling Li <xil226@ucsd.edu>
2016-11-07 23:31:35 -0800

    check isAlive and levelup

--- creature.py
+++ creature.py
@@ -35,7 +35,9 @@ class Creature(object):
 		self.creature = canvas.create_oval(self.x1,self.y1,self.x2,self.y2,fill="black")
 
 	"""
-	Move method: creature moves to certain position based on the move scope
+	Move method: creature moves to certain position based on the move scope.
+
+	A main activation function for the creature when it is alive until it is dead.
 
     In version 1.1, move strategy is random by generator
 
@@ -43,8 +45,12 @@ class Creature(object):
     """
 	def move(self,delay,canvas):
 		while True:
+		  if self.isAlive() == 0:
+		  	canvas.delete(self.creature)
+		  	break
 		  horizontalRandChoice = random.randint(1,10)
 		  verticalRandChoice = random.randint(1,10)
+		  randScope = random.randint(1,self.moveScope)
 		  horizontalMove = 0
 		  verticalMove = 0
 
@@ -62,17 +68,38 @@ class Creature(object):
 		  x2old = self.x2
 		  y2old = self.y2
 
-		  self.x1 = self.x1 + horizontalMove * self.cellWidth
-		  self.y1 = self.y1 + verticalMove * self.cellHeight
-		  self.x2 = self.x2 + horizontalMove * self.cellWidth
-		  self.y2 = self.y2 + verticalMove * self.cellHeight
-		  canvas.coords(self.creature,self.x1,self.y1,self.x2,self.y2)
+		  self.x1 = self.x1 + horizontalMove * self.moveScope * self.cellWidth
+		  self.y1 = self.y1 + verticalMove * self.moveScope * self.cellHeight
+		  self.x2 = self.x2 + horizontalMove * self.moveScope * self.cellWidth
+		  self.y2 = self.y2 + verticalMove * self.moveScope * self.cellHeight
 
 		  if self.x1 < 0 or self.y1 < 0 or self.x2 > self.worldWidth or self.y2 > self.worldHeight:
 		  	self.x1 = x1old
 		  	self.y1 = y1old
 		  	self.x2 = x2old
 		  	self.y2 = y2old
+		  	self.hp -= 10
 		  else:
+		  	canvas.coords(self.creature,self.x1,self.y1,self.x2,self.y2)
+		  	self.levelUp()
 		  	break
-		self.world.after(delay, lambda: self.move(delay,canvas)) 	
+		self.world.after(delay, lambda: self.move(delay,canvas)) 
+
+	def levelUp(self):
+	  self.experience += 10
+	  if self.experience == 1000:
+	    self.level += 1
+	    self.power += 2
+	    self.hp += 20
+	    self.experience = 0
+	  if self.level % 10 == 0:
+	  	if self.moveScope < 5:
+	  	  self.moveScope += 1
+	  	if self.visionScope < 10:
+	  	  self.visionScope += 1
+
+	def isAlive(self):
+		if self.hp <= 0:
+			return 0
+		else:
+		    return 1 
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import tkinter as tk
 from world import World
 
 def main():
-	newWorld = World(30,30)
+	newWorld = World(100,100)
 	newWorld.createWorld()
-	newWorld.addCreature(1,1)
-	newWorld.creatures[1,1].move(500,newWorld.space)
+	newWorld.addCreature(50,50)
+	newWorld.creatures[50,50].move(1000,newWorld.space)
 	newWorld.world.mainloop()
 
 main()
